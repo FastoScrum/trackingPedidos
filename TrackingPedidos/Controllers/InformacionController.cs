bb4a3d95a17d7756f7087697fbb73fcdceeb7e04
Sebastián Echeverría <esecheverriab@utn.edu.ec>
2019-02-03 22:25:50 -0500

    Despliegue en Heroku

---
+++ TrackingPedidos/Controllers/InformacionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using TrackingPedidos.Models;
+
+namespace TrackingPedidos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InformacionController : ControllerBase
+    {
+        private readonly TrackingContext _context;
+
+        public InformacionController(TrackingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Informacion/548484848
+        [HttpGet("{factura}")]
+        public async Task<IActionResult> FindByFactura(string factura)
+        {
+            try
+            {
+                var pedido = await _context.Pedidos.FirstAsync(i => i.InvoiceNumber == factura);
+                if (pedido == null)
+                {
+                    return NotFound();
+                }
+                return Ok(pedido);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
