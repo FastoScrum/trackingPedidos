297c3b6129294fd41ca358b5e33f5cc7b0fc58f5
Mauricio Chiriboga <machdeto@gmail.com>
2019-02-18 21:42:04 -0500

    Dashboard

--- TrackingPedidos/Controllers/DashboardController.cs
+++ TrackingPedidos/Controllers/DashboardController.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TrackingPedidos.Models;
 
 namespace TrackingPedidos.Controllers
 {
     [Authorize]
     public class DashboardController : Controller
     {
+        private readonly TrackingContext _context;
+
+        public DashboardController(TrackingContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            if (User.IsInRole("Administrador"))
+            {
+                ViewData["Pendientes"] = _context.Pedidos.Where(i => i.PedFase.ToString() == "P").Count();
+                ViewData["EnCamino"] = _context.Pedidos.Where(i => i.PedFase.ToString() == "C").Count();
+                ViewData["EnDistribuidora"] = _context.Pedidos.Where(i => i.PedFase.ToString() == "F").Count();
+                ViewData["Entregados"] = _context.Pedidos.Where(i => i.PedFase.ToString() == "E").Count();
+                ViewData["NoEntregados"] = _context.Pedidos.Where(i => i.PedFase.ToString() == "N").Count();
+            }
             return View();
         }
     }
